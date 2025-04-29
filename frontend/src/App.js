2aa4862dacc911cdebf7eb2b583fafad3c424413
M. Sakeef Hossain <sakeef02@gmail.com>
2025-04-29 11:35:21 +0600

    added route for admin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Receptionist from "./pages/receptionist-pages/Receptionist.js";
+import Login from "./pages/login-pages/Login.js";
+import Checkout from "./pages/receptionist-pages/Checkout.js";
+import Features from "./pages/receptionist-pages/feature.js";
+import RealCheckout from "./pages/receptionist-pages/RealCheckout.js";
+import RPDash from "./pages/receptionist-pages/ReceptionistDB.js";
+import ManDash from "./pages/manager-pages/ManagerDash.js";
+import Employee from "./pages/manager-pages/Employee.js";
+import Manexpenses from "./pages/manager-pages/Manexpenses.js";
+import AdminDashBoard from "./pages/admin-pages/AdminDashBoard.js";
+import UpdateManager from "./pages/admin-pages/UpdateManager.js";
+import AdminHotel from "./pages/admin-pages/AdminHotel.js";
+import ManRoom from "./pages/manager-pages/Rooms.js";
+import Inventory from "./pages/manager-pages/Inventory.js";
+import Ledgerbook from "./pages/manager-pages/Ledgerbook";
+import Hotelist from "./pages/admin-pages/Hotelist";
+import FinancialReport from "./pages/admin-pages/FinancialReport";
+import MaintenanceLedgerPage from "./pages/manager-pages/BillLedger";
+
+const PrivateRoute = ({ element, role }) => {
+  const storedRole = localStorage.getItem("role");
+  return storedRole === role ? element : <Navigate to="/login" />;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <title>Hotel Management System</title>
+      <Routes>
+        
+       
+      
+        
+       
+        {/* Admin Pages */}
+        <Route path="/admin-dashboard" element={<PrivateRoute element={<AdminDashBoard/>} role="admin"/>} />
+        <Route path="/update-managers" element={<PrivateRoute element={<UpdateManager/>} role="admin"/>} />
+        <Route path="/admin-hotels" element={<PrivateRoute element={<AdminHotel/>} role="admin"/>} />
+        <Route path="/all-expenses" element={<PrivateRoute element={<Hotelist/>} role="admin"/>} />
+        <Route path="/financial-report/:hotelId" element={<PrivateRoute element={<FinancialReport/>} role="admin"/>} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
